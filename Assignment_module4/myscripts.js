de235b809a520d3bcc95e47b44b9f3c9f948616d
Nashit Akhanda <62715159+Akhanda99@users.noreply.github.com>
2020-04-22 14:50:05 +0600

    Create myscripts.js

---
+++ Assignment_module4/myscripts.js
@@ -0,0 +1,10 @@
+var names=["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
+        for(var i=0; i<names.length;i++){
+            if(names[i][0]=="j" || names[i][0]=="J"){
+                console.log("Good Bye "+ names[i]);
+            }
+            else{
+                console.log("Hello "+ names[i]);
+            }
+            
+        }
